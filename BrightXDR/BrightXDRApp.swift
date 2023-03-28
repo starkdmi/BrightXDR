6404876a22364976861b28e4a5ab841fbce43721
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-03-28 13:36:44 +0300

    Initial Commit

---
+++ BrightXDR/BrightXDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightXDRApp.swift
+//  BrightXDR
+//
+//  Created by Dmitry Starkov on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrightXDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
